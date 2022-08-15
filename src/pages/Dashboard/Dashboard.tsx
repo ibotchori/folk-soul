edf6082e6f10aa49a8bf4e8e434bc4158d6277ae
Irakli <i.botchori@gmail.com>
2022-08-15 22:58:25 +0400

    style: add content on Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,12 +4,17 @@ type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <div className='flex justify-between'>
-      <div className='w-24 h-56 bg-orange-600'>Side bar</div>
+    <div className='flex justify-end items-center h-screen md:mr-32 mr-16'>
       {/* Content */}
-      <div className='w-3/4 h-[70vh] bg-white rounded-md flex flex-col justify-between'>
-        <p>დილამშვიდობისა</p>
-        <p>დილამშვიდობისა</p>
+      <div className='w-4/6 h-[80vh] bg-white rounded-md flex flex-col justify-evenly  items-center'>
+        <p className='font-[mtavruli]  text-2xl lg:text-5xl tracking-widest '>
+          დილამშვიდობისა!
+        </p>
+        <img
+          className=''
+          src='https://www.slazzer.com/static/images/home-page/banner-orignal-image.jpg'
+          alt=''
+        />
       </div>
     </div>
   )
