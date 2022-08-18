3055abcc77fdfb5f5b65a79194527c705cf3e3c2
Irakli <i.botchori@gmail.com>
2022-08-18 17:19:27 +0400

    style: add avatar on members card

--- src/pages/Members/Members.tsx
+++ src/pages/Members/Members.tsx
@@ -1,3 +1,4 @@
+import { EditPhotoIcon } from 'assets'
 import React from 'react'
 
 type Props = {}
@@ -13,23 +14,68 @@ const Members = (props: Props) => {
         </p>
         {/* Members */}
         <div className='flex justify-between w-[70%]'>
-          <div className='bg-sidebarColor w-[13.5rem] h-[17.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-white flex flex-col justify-between items-center'>
-            <div>Image</div>
-            <div>Title</div>
+          <div className='bg-sidebarColor w-[13.5rem] h-[17.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-gray-200 flex flex-col justify-between pt-8 items-center'>
+            <div className='relative w-36 h-36  '>
+              {/* Avatar Image */}
+              <img
+                className='w-36 h-36 rounded-full border-2 border-gray-200 '
+                src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'
+                alt='avatar'
+              />
+
+              {/* Avatar Edit Button */}
+              <img
+                className='absolute bottom-0 right-0 cursor-pointer pr-3'
+                src={EditPhotoIcon}
+                alt='Edit'
+              />
+            </div>
+
+            <p className='font-[mtavruli]'>საბა</p>
             <div className=' w-full text-center h-10 border-t-2 border-black'>
               Button
             </div>
           </div>
-          <div className='bg-sidebarColor w-[13.5rem] h-[17.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-white flex flex-col justify-between items-center'>
-            <div>Image</div>
-            <div>Title</div>
+          <div className='bg-sidebarColor w-[13.5rem] h-[17.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-gray-200 flex flex-col justify-between pt-8 items-center'>
+            <div className='relative w-36 h-36  '>
+              {/* Avatar Image */}
+              <img
+                className='w-36 h-36 rounded-full border-2 border-gray-200 '
+                src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'
+                alt='avatar'
+              />
+
+              {/* Avatar Edit Button */}
+              <img
+                className='absolute bottom-0 right-0 cursor-pointer pr-3'
+                src={EditPhotoIcon}
+                alt='Edit'
+              />
+            </div>
+
+            <p className='font-[mtavruli]'>ვანო</p>
             <div className=' w-full text-center h-10 border-t-2 border-black'>
               Button
             </div>
           </div>
-          <div className='bg-sidebarColor w-[13.5rem] h-[17.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-white flex flex-col justify-between items-center'>
-            <div>Image</div>
-            <div>Title</div>
+          <div className='bg-sidebarColor w-[13.5rem] h-[17.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-gray-200 flex flex-col justify-between pt-8 items-center'>
+            <div className='relative w-36 h-36  '>
+              {/* Avatar Image */}
+              <img
+                className='w-36 h-36 rounded-full border-2 border-gray-200 '
+                src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'
+                alt='avatar'
+              />
+
+              {/* Avatar Edit Button */}
+              <img
+                className='absolute bottom-0 right-0 cursor-pointer pr-3'
+                src={EditPhotoIcon}
+                alt='Edit'
+              />
+            </div>
+
+            <p className='font-[mtavruli]'>ნიკა</p>
             <div className=' w-full text-center h-10 border-t-2 border-black'>
               Button
             </div>
