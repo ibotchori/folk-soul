6b3036cc776818ff6e031f8c7ea645027cc9eb6a
Irakli <i.botchori@gmail.com>
2022-08-18 19:17:24 +0400

    style: add new member button on member's card

--- src/pages/Members/Members.tsx
+++ src/pages/Members/Members.tsx
@@ -9,7 +9,7 @@ const Members = (props: Props) => {
       {/* Content */}
       <div className='w-4/6  sm:h-[60vh] md:h-[80vh]  bg-[#FBFBFB] rounded-2xl flex flex-col justify-evenly items-center shadow-[inset_8px_8px_10px_0_rgba(0,0,0,0.3)]'>
         {/* Title */}
-        <p className='font-[mtavruli] text-xl  tracking-widest  w-[80%] text-center border-b-2 border-gray-600 pb-5'>
+        <p className='font-[mtavruli] text-lg  tracking-widest  w-[80%] text-center border-b-2 border-gray-600 pb-5 '>
           ჯგუფის წევრები
         </p>
         {/* Members */}
@@ -111,6 +111,14 @@ const Members = (props: Props) => {
             </div>
           </div>
         </div>
+
+        {/* Add new member button */}
+        <button>
+          {' '}
+          <p className='font-[mtavruli] text-[#3A7DA3] underline tracking-widest font-bold text-lg'>
+            ახალი წევრი გყავს?
+          </p>
+        </button>
       </div>
     </div>
   )
