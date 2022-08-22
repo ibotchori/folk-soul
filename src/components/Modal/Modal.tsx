da37762575648754878f9b9c302beb5bc91dcea9
Irakli <i.botchori@gmail.com>
2022-08-22 17:15:29 +0400

    style: add hover effect on modal close buttons

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -18,7 +18,11 @@ const Modal: React.FC<ModalProps> = ({ visible, onClose }) => {
             onClick={onClose}
             className='w-full flex justify-end pr-4 top-0 pt-4 absolute'
           >
-            <img className='md:w-7 w-4' src={CloseIcon} alt='' />
+            <img
+              className='md:w-7 w-4 hover:bg-gray-300  rounded-full'
+              src={CloseIcon}
+              alt='Close Icon'
+            />
           </button>
           {/* Title */}
           <p className='font-[mtavruli] w-[85%] text-center text-xs md:text-lg pt-14 tracking-widest border-b-2 border-gray-400 '>
@@ -35,7 +39,7 @@ const Modal: React.FC<ModalProps> = ({ visible, onClose }) => {
           </div>
           {/* Upload button */}
           <button className=' w-32 md:w-40 h-10 bg-[#143B52] rounded-lg mb-5 md:mb-20 '>
-            <p className='font-[mtavruli] text-center text-sm md:text-lg pt-1 tracking-widest text-gray-200  '>
+            <p className='font-[mtavruli] text-center text-sm md:text-lg pt-1 tracking-widest text-gray-200 hover:text-gray-300    '>
               ატვირთე
             </p>
           </button>
