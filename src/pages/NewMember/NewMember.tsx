c7596e8c19265caa4494c2356677e4e99c2758d3
Irakli <i.botchori@gmail.com>
2022-08-26 10:25:44 +0400

    style: fix mobile view on NewMember form

--- src/pages/NewMember/NewMember.tsx
+++ src/pages/NewMember/NewMember.tsx
@@ -8,14 +8,14 @@ const NewMember = (props: Props) => {
   return (
     <div className='flex justify-center md:justify-end items-center h-screen md:mr-24 w-full '>
       {/* Content */}
-      <div className='w-[77%]  h-[70vh] md:h-[80vh]  bg-[#FBFBFB] rounded-2xl flex flex-col justify-evenly items-center shadow-[inset_8px_8px_10px_0_rgba(0,0,0,0.3)]'>
+      <div className='w-[77%]  h-[70vh] md:h-[80vh]  bg-[#FBFBFB] rounded-2xl flex flex-col justify-between items-center shadow-[inset_8px_8px_10px_0_rgba(0,0,0,0.3)]'>
         {/* Title */}
-        <p className='font-[mtavruli] text-lg  tracking-widest  w-[80%] text-center border-b-2 border-gray-600 pb-5 '>
+        <p className='font-[mtavruli] text-xs md:text-lg  tracking-widest pt-10  w-[80%] text-center border-b-2 border-gray-600 pb-5 '>
           დაამატე ჯგუფის ახელი წევრი
         </p>
         {/* Form */}
-        <div className='flex flex-col gap-3 sm:gap-12 items-center'>
-          <div className='w-[50%]'>
+        <div className='flex flex-col gap-3 md:gap-8 items-center w-full lg:w-[60%]'>
+          <div className=' w-[60%] md:w-[40%]'>
             <input
               type='text'
               id='name'
@@ -24,7 +24,7 @@ const NewMember = (props: Props) => {
               required
             />
           </div>
-          <div className='flex gap-3 sm:gap-10 w-[80%]'>
+          <div className='flex gap-1 md:gap-10 w-[90%] md:w-[70%]'>
             <input
               type='text'
               id='instrument'
@@ -40,16 +40,16 @@ const NewMember = (props: Props) => {
               required
             />
             <input
-              type='text'
+              type='color'
               id='color'
-              className='bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500   focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center'
+              className='bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500   focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center h-14'
               placeholder='ფერი'
               required
             />
           </div>
-          <div className='w-[95%]'>
+          <div className='w-[95%] md:w-[80%]'>
             <textarea
-              rows={6}
+              rows={8}
               id='info'
               className='  bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2 '
               placeholder='ბიოგრაფია'
@@ -59,7 +59,7 @@ const NewMember = (props: Props) => {
         </div>
 
         {/* Add new member button */}
-        <div className='flex flex-col'>
+        <div className='flex flex-col gap-3 pb-3 md:pb-10'>
           <Button text='დაამატე წევრი' backgroundColor='primaryBG' />
           <GoBackButton />
         </div>
