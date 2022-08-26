728e15182905c66502d543363fa96ae06113928e
Irakli <i.botchori@gmail.com>
2022-08-26 09:38:46 +0400

    style: add style on NewMember's inputs

--- src/pages/NewMember/NewMember.tsx
+++ src/pages/NewMember/NewMember.tsx
@@ -14,11 +14,48 @@ const NewMember = (props: Props) => {
           დაამატე ჯგუფის ახელი წევრი
         </p>
         {/* Form */}
-        <div>
-          <input className='border border-gray-500' type='text' />
-          <input className='border border-gray-500' type='text' />
-          <input className='border border-gray-500' type='color' />
-          <input className='border border-gray-500' type='text' />
+        <div className='flex flex-col gap-3 sm:gap-12 items-center'>
+          <div className='w-[50%]'>
+            <input
+              type='text'
+              id='name'
+              className='bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center'
+              placeholder='სახელი'
+              required
+            />
+          </div>
+          <div className='flex gap-3 sm:gap-10 w-[80%]'>
+            <input
+              type='text'
+              id='instrument'
+              className='bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center'
+              placeholder='ინსტრუმენტი'
+              required
+            />
+            <input
+              type='number'
+              id='orbit'
+              className='bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center'
+              placeholder='ორბიტის სიგანე'
+              required
+            />
+            <input
+              type='text'
+              id='color'
+              className='bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500   focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center'
+              placeholder='ფერი'
+              required
+            />
+          </div>
+          <div className='w-[95%]'>
+            <textarea
+              rows={6}
+              id='info'
+              className='  bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2 '
+              placeholder='ბიოგრაფია'
+              required
+            ></textarea>
+          </div>
         </div>
 
         {/* Add new member button */}
