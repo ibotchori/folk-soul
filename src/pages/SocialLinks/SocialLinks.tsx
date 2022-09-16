7ed21a6bdd8956f37b32106a3430cc8afb683bb2
Irakli <i.botchori@gmail.com>
2022-09-16 11:51:25 +0400

    style: add HTML on SocialLink Page

--- src/pages/SocialLinks/SocialLinks.tsx
+++ src/pages/SocialLinks/SocialLinks.tsx
@@ -3,7 +3,23 @@ import React from 'react'
 type Props = {}
 
 const SocialLinks = (props: Props) => {
-  return <div>SocialLinks</div>
+  return (
+    <div className='flex justify-center md:justify-end items-center h-screen md:mr-24 w-full '>
+      {/* Content */}
+      <div className='w-[77%]  h-[70vh] md:h-[80vh]  bg-[#FBFBFB] rounded-2xl flex flex-col justify-evenly items-center shadow-[inset_8px_8px_10px_0_rgba(0,0,0,0.3)]'>
+        {/* Title */}
+        <p className='font-[mtavruli] text-lg  tracking-widest  w-[80%] text-center border-b-2 border-gray-600 pb-5 '>
+          სოციალური ბმულები
+        </p>
+        {/* Social Links */}
+
+        {/* Add new member button */}
+        <button className='font-[mtavruli] hover:text-[#55a5d3] text-[#3A7DA3] underline tracking-widest font-bold text-lg'>
+          დაამატე ახალი სოციალური ბმული
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default SocialLinks
