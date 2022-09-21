9e08a1dad101db17d22f678cb73abddca5788108
Irakli <i.botchori@gmail.com>
2022-09-21 15:58:40 +0400

    style: new social link page

--- src/components/Button/GoBackButton.tsx
+++ src/components/Button/GoBackButton.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-type Props = {}
+type GoButtonProps = {
+  className?: string
+}
 
-const GoBackButton = (props: Props) => {
+const GoBackButton: React.FC<GoButtonProps> = ({ className }) => {
   const navigate = useNavigate()
   return (
     <button
       onClick={() => navigate(-1)}
-      className='font-[mtavruli] hover:text-[#55a5d3] text-[#3A7DA3] underline tracking-widest font-bold text-lg'
+      className={`font-[mtavruli] hover:text-[#55a5d3] text-[#3A7DA3] underline tracking-widest font-bold text-lg ${className}`}
     >
       გადი უკან
     </button>
--- src/pages/NewSocialLink/NewSocialLink.tsx
+++ src/pages/NewSocialLink/NewSocialLink.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Button from 'components/Button/Button'
+import GoBackButton from 'components/Button/GoBackButton'
+import Input from 'components/Input/Input'
 
 type Props = {}
 
 const NewSocialLink = (props: Props) => {
-  return <div>NewSocialLink</div>
+  return (
+    <div className='flex justify-center md:justify-end items-center h-screen md:mr-24 w-full '>
+      {/* Content */}
+      <div className='w-[77%]  h-[70vh] md:h-[80vh]  bg-[#FBFBFB] rounded-2xl flex flex-col justify-between items-center shadow-[inset_8px_8px_10px_0_rgba(0,0,0,0.3)]'>
+        {/* Title */}
+        <p className='font-[mtavruli] text-xs md:text-lg  tracking-widest pt-10  w-[80%] text-center border-b-2 border-gray-600 pb-5 '>
+          დაამატე სოციალური ბმული
+        </p>
+        {/* Form */}
+        <div className='flex flex-col gap-3 md:gap-8 items-center w-full lg:w-[60%]'>
+          <div className=' w-[60%] md:w-[40%]'>
+            <Input id='name' type='text' placeholder='სახელი' />
+          </div>
+          <div className=' w-[60%] md:w-[70%]'>
+            <Input id='name' type='text' placeholder='ბმული' />
+          </div>
+        </div>
+        {/* Add new member button */}
+        <div className='flex flex-col gap-3 pb-3 md:pb-40'>
+          <Button
+            text='დაამატე სოციალური ბმული'
+            backgroundColor='bg-primaryBG'
+          />
+          <GoBackButton className='pt-8' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NewSocialLink
