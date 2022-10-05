56f8ff9214e6e6355010dc3a85172c5c64d4d19a
Irakli <i.botchori@gmail.com>
2022-10-05 21:52:46 +0400

    chore: add className props on Title component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 type TitleProps = {
   text: string
+  className?: string
 }
 
-const Title: React.FC<TitleProps> = ({ text }) => {
+const Title: React.FC<TitleProps> = ({ text, className }) => {
   return (
-    <p className='font-[mtavruli] text-lg  tracking-widest  w-[80%] text-center border-b-2 border-gray-600 pb-5 '>
+    <p
+      className={`${className} font-[mtavruli] text-lg  tracking-widest  w-[80%] text-center border-b-2 border-gray-600 pb-5 `}
+    >
       {text}
     </p>
   )
