e3711d14681e36b4e8d04d2eeab2a19196aab97b
Irakli <i.botchori@gmail.com>
2022-10-17 16:07:00 +0400

    feat: add height props on Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 type ButtonProps = {
   text: string
   backgroundColor: string
+  height?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ text, backgroundColor }) => {
+const Button: React.FC<ButtonProps> = ({ text, backgroundColor, height }) => {
   return (
     <button
-      className={`px-8 h-12 ${backgroundColor} rounded-lg  font-[mtavruli] text-center text-sm md:text-lg pt-1 tracking-widest text-gray-200 hover:text-gray-100`}
+      className={`px-8 h-12 ${backgroundColor} ${height} rounded-lg  font-[mtavruli] text-center text-sm md:text-lg pt-1 tracking-widest text-gray-200 hover:text-gray-100`}
     >
       {text}
     </button>
