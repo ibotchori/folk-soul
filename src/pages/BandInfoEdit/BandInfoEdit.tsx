4d6588f7d314b9d280be84b0ae218f21f3d8a977
Irakli <i.botchori@gmail.com>
2022-10-21 13:44:50 +0400

    style: correct style on BandInfoEdit page

--- src/pages/BandInfoEdit/BandInfoEdit.tsx
+++ src/pages/BandInfoEdit/BandInfoEdit.tsx
@@ -9,16 +9,16 @@ const BandInfoEdit = (props: Props) => {
       {/* Content */}
       <div className='w-[77%]  h-[70vh] md:h-[80vh]  bg-[#FBFBFB] rounded-2xl flex flex-col justify-between items-center shadow-[inset_8px_8px_10px_0_rgba(0,0,0,0.3)]'>
         {/* Title */}
-        <Title
-          className='pt-20 text-lg pb- !w-full'
-          text='ბენდის შესახებ - დაარედაქტირე'
-        />
-
-        <div className=' w-[80%] mb-8  h-[460px] bg-[#E5E5E5] rounded-lg shadow-[5px_5px_15px_5px_rgba(0,0,0,0.35)]'>
-          {' '}
+        <div className=' w-[80%] '>
+          <Title
+            className='pt-20 text-lg pb- !w-full'
+            text='ბენდის შესახებ - დაარედაქტირე'
+          />
         </div>
 
-        <div className=' flex flex-col items-center justify-center pb-'>
+        <div className=' w-[80%] mb-8 mt-5 h-[460px] bg-[#E5E5E5] rounded-lg shadow-[5px_5px_15px_5px_rgba(0,0,0,0.35)]'></div>
+
+        <div className=' flex flex-col items-center justify-center pb-5'>
           <Button text='შეინახე' className='bg-[#53C02C] h-10 mb-4 px-12' />
           <LinkButton text='გადი უკან' path={'/band-info'} />
         </div>
