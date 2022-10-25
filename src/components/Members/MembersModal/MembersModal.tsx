3dfd24542f3c61c1bcec429b3940478d635fcd9d
Irakli <i.botchori@gmail.com>
2022-10-25 12:56:03 +0400

    style: add html elements on MembersModal component

--- src/components/Members/MembersModal/MembersModal.tsx
+++ src/components/Members/MembersModal/MembersModal.tsx
@@ -23,7 +23,7 @@ const MembersModal: React.FC<MembersModalProps> = ({
     <div className='fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm flex items-center justify-center'>
       <div className='flex justify-center items-center h-screen w-full '>
         {/* Content */}
-        <div className='md:max-w-[30%] w-[70%] h-[50vh]  md:h-[60vh] pb-10 md:pb-20 bg-[#FBFBFB] rounded-xl flex flex-col justify-between items-center relative '>
+        <div className='md:max-w-[30%] w-[70%] h-[50vh]  md:h-[60vh] pb-10 md:pb-20 bg-[#FBFBFB] rounded-xl flex flex-col justify-between items-center relative text-center '>
           {/* Modal close button */}
           <button
             onClick={onClose}
@@ -36,20 +36,31 @@ const MembersModal: React.FC<MembersModalProps> = ({
             />
           </button>
           {/* Title */}
-          <p className='font-[mtavruli] w-[85%] text-center text-xs md:text-lg pt-14 tracking-widest border-b-2 border-gray-400 '>
+          <p className='font-[mtavruli] w-[85%] text-center text-xs md:text-lg pt-14 tracking-widest border-b-2 border-gray-400  '>
             {title}
           </p>
-          {/* Sub Title */}
-          <p className=''>{subTitle}</p>
 
           {/* Avatar Image */}
-          <div className='relative pb-8'>
+          <div className='relative py-6'>
             <img
               className={`shadow-[15px_15px_15px_rgba(0,0,0,0.35)]  md:w-52 w-32  md:h-52 h-32  border-2 border-gray-200 ${className}`}
               src={image}
               alt='avatar'
             />
           </div>
+          {/* Sub Title */}
+          <h4 className='pb-6 font-[mtavruli]'>ორბიტიალური დაშორება: {12}</h4>
+          <p className=''>
+            თეთრი დათვის ხშირი ბეწვი ძალიან ღია ყვითელი ფერისაა იმისათვის, რომ
+            შეერწყას თოვლიან გარემოს. კიდურების მიდამოებში ბეწვი გრძელია, რაც
+            ეხმარება დათვს სრიალა ყინულზე გადაადგილებისას. თეთრ დათვს დიდი
+            მანძილის გავლა შეუძლია. თეთრი დათვის ხშირი ბეწვი ძალიან ღია ყვითელი
+            ფერისაა იმისათვის, რომ შეერწყას თოვლიან გარემოს. კიდურების
+            მიდამოებში ბეწვი გრძელია, რაც ეხმარება დათვს სრიალა ყინულზე
+            გადაადგილებისას. თეთრ დათვს დიდი მანძილის გავლა შეუძლია. თეთრი
+            დათვის ხშირი ბეწვი ძალიან ღია ყვითელი ფერისაა იმისათვის, რომ
+            შეერწყას თოვლიან გარემოს.
+          </p>
         </div>
       </div>
     </div>
