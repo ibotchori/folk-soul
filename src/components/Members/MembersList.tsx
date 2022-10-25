394f4a32c9a20d0844a8a101efa44172f56ecad7
Irakli <i.botchori@gmail.com>
2022-10-25 12:48:24 +0400

    chore: change Modal functionality on MembersList component

--- src/components/Members/MembersList.tsx
+++ src/components/Members/MembersList.tsx
@@ -19,9 +19,7 @@ type Props = {}
 const MembersList = (props: Props) => {
   /* Modal Functionality */
   const [showModal, setShowModal] = useState(false)
-  const handleOnClose = () => {
-    setShowModal(false)
-  }
+  const [showMembersModal, setShowMembersModal] = useState(false)
   /* END Modal Functionality */
 
   // Imported  Hook
@@ -36,39 +34,48 @@ const MembersList = (props: Props) => {
     .slice(pageVisited, pageVisited + membersPerPage)
     .map((member) => {
       return (
-        <div className='bg-sidebarColor sm:w-[13.5rem] w-[10.5rem] sm:h-[17.375rem] h-[14.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-gray-200 flex flex-col justify-between pt-8 items-center'>
-          <div className='relative'>
-            {/* Avatar Image */}
-            <img
-              className='sm:w-36 w-32  sm:h-36 h-32  rounded-full border-2 border-gray-200 '
-              src={DefaultMemberAvatar}
-              alt='avatar'
-            />
+        <>
+          <div className='bg-sidebarColor sm:w-[13.5rem] w-[10.5rem] sm:h-[17.375rem] h-[14.375rem] rounded-md shadow-[15px_15px_15px_rgba(0,0,0,0.35)] border-2 border-black text-gray-200 flex flex-col justify-between pt-8 items-center'>
+            <div className='relative'>
+              {/* Avatar Image */}
+              <img
+                className='sm:w-36 w-32  sm:h-36 h-32  rounded-full border-2 border-gray-200 '
+                src={DefaultMemberAvatar}
+                alt='avatar'
+              />
 
-            {/* Avatar Edit Button */}
-            <img
-              onClick={() => setShowModal(true)}
-              className='absolute bottom-0 right-0 cursor-pointer pr-3'
-              src={EditPhotoIcon}
-              alt='Edit'
-            />
-          </div>
+              {/* Avatar Edit Button */}
+              <img
+                onClick={() => setShowModal(true)}
+                className='absolute bottom-0 right-0 cursor-pointer pr-3'
+                src={EditPhotoIcon}
+                alt='Edit'
+              />
+            </div>
 
-          <p className='font-[mtavruli]'>{member.title}</p>
-          <div className=' w-full text-center h-10 border-t-2 border-black'>
-            <div className='flex justify-between px-5 pt-2 items-center'>
-              <button onClick={() => setShowModal(true)}>
-                <img src={SeeIcon} alt='See Icon' />
-              </button>
-              <button>
-                <img src={ModifyIcon} alt='Modify Icon' />
-              </button>
-              <button>
-                <img src={DeleteIcon} alt='Delete Icon' />
-              </button>
+            <p className='font-[mtavruli]'>{member.title}</p>
+            <div className=' w-full text-center h-10 border-t-2 border-black'>
+              <div className='flex justify-between px-5 pt-2 items-center'>
+                <button onClick={() => setShowMembersModal(true)}>
+                  <img src={SeeIcon} alt='See Icon' />
+                </button>
+                <button>
+                  <img src={ModifyIcon} alt='Modify Icon' />
+                </button>
+                <button>
+                  <img src={DeleteIcon} alt='Delete Icon' />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+          <MembersModal
+            onClose={() => setShowMembersModal(false)}
+            image={DefaultMemberAvatar}
+            className='rounded-full'
+            visible={showMembersModal}
+            title={'ტესტ'}
+          />
+        </>
       )
     })
 
@@ -90,14 +97,7 @@ const MembersList = (props: Props) => {
         nextLinkClassName={'nextButton'}
       />
       <Modal
-        onClose={handleOnClose}
-        image={DefaultMemberAvatar}
-        className='rounded-full'
-        visible={showModal}
-        title={'შეცვალე ჯგუფის წევრის ავატარი'}
-      />
-      <MembersModal
-        onClose={handleOnClose}
+        onClose={() => setShowModal(false)}
         image={DefaultMemberAvatar}
         className='rounded-full'
         visible={showModal}
