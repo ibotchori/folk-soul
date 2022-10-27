474aff8d6953c1192c086f72bc5e2c7b96b547a3
Irakli <i.botchori@gmail.com>
2022-10-27 11:44:19 +0400

    style: fix input style

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,7 +11,7 @@ const Input: React.FC<InputProps> = ({ id, type, placeholder }) => {
     <input
       type={type}
       id={id}
-      className='h-14 bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2 placeholder:text-center text-center'
+      className='h-14  text-black bg-[#C3B6B2] text-sm  rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2  placeholder:text-black '
       placeholder={placeholder}
       required
     />
