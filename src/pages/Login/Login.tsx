813d98af337da4f10f100f05d71c7a1645fa0685
Irakli <i.botchori@gmail.com>
2022-10-27 11:47:31 +0400

    chore: navigate to dashboard page on from login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,13 @@
 import { Input } from 'components'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 
 const Login = (props: Props) => {
   return (
     <div className='  items-center justify-center flex w-full h-full'>
-      <div className='bg-gradient-to-t  from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
+      <div className='bg-gradient-to-t px-14 from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
         <div className='bg-[#EB5757] w-40 h-12 text-center skew-x-[-40deg]'>
           <p className='skew-x-[40deg] font-[mtavruli] font-bold pt-3 pr-2 text-md'>
             კარიბჭე
@@ -14,9 +15,12 @@ const Login = (props: Props) => {
         </div>
         <Input id='username' placeholder='მეტსახელი' type='text' />
         <Input id='password' placeholder='პაროლი' type='password' />
-        <button className='bg-[#345161] border rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli] pt-1 tracking-wider'>
+        <Link
+          to={'/dashboard'}
+          className='bg-[#345161] border pt-4 rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli]  tracking-wider'
+        >
           შემობრძანდი
-        </button>
+        </Link>
       </div>
     </div>
   )
