0374bdad1ce743b2209d52f63f27e669fb38d29c
Irakli Botchorishvili <i.botchori@gmail.com>
2022-10-31 22:54:41 +0400

    chore: remove test reducer from Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,21 +1,10 @@
 import { Input } from 'components'
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { useAppSelector, useAppDispatch } from '../../app/hooks'
-import {
-  incrementByAmount,
-  selectCount,
-} from '../../features/counter/counterSlice'
 
 type Props = {}
 
 const Login = (props: Props) => {
-  const count = useAppSelector(selectCount)
-  console.log('🚀 ~ Login ~ count', count)
-  const dispatch = useAppDispatch()
-  const [incrementAmount, setIncrementAmount] = useState('2')
-
-  const incrementValue = Number(incrementAmount) || 0
   return (
     <div className='  items-center justify-center flex w-full h-full'>
       <div className='bg-gradient-to-t px-14 from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
@@ -27,12 +16,7 @@ const Login = (props: Props) => {
         <Input id='username' placeholder='მეტსახელი' type='text' />
         <Input id='password' placeholder='პაროლი' type='password' />
 
-        <input
-          onChange={(e) => setIncrementAmount(e.target.value)}
-          type='number'
-        />
         <Link
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
           to={'/dashboard'}
           className='bg-[#10191d] border pt-4 rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli]  tracking-wider'
         >
