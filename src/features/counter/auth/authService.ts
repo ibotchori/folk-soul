c4f6528d47ceb1eabaacf3c4a4ee95e86444e8cb
Irakli Botchorishvili <i.botchori@gmail.com>
2022-10-31 22:35:20 +0400

    wip: api call from authService

--- src/features/counter/auth/authService.ts
+++ src/features/counter/auth/authService.ts
@@ -1,1 +1,8 @@
-export {}
+import axios from 'axios'
+
+export const getAllMembers = async () => {
+  const response = await axios.get(
+    'https://folk-soul-api.ibotchori.space/api/band-member/get-all'
+  )
+  return response.data
+}
