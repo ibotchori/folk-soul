7c562bf88841577c6153b5698d21685793df8e0a
Irakli <i.botchori@gmail.com>
2022-11-01 16:44:59 +0400

    wip: add useFrom on Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,25 @@
 import { Input } from 'components'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { LoginInputTypes, LoginSchema } from 'Schema/LoginSchema'
 
 type Props = {}
 
 const Login = (props: Props) => {
+  // useForm
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors, dirtyFields },
+  } = useForm<LoginInputTypes>({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    resolver: yupResolver(LoginSchema),
+  })
   return (
     <div className='  items-center justify-center flex w-full h-full'>
       <div className='bg-gradient-to-t px-14 from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
