73fe9a874b31e3e861d36d41a83d1c045737d983
Irakli <i.botchori@gmail.com>
2022-11-01 16:47:31 +0400

    wip: add props on Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,9 +4,21 @@ type InputProps = {
   id: string
   type: string
   placeholder: string
+  errorMessage?: string
+  name?: string
+  register?: any
+  dirtyFields?: boolean
 }
 
-const Input: React.FC<InputProps> = ({ id, type, placeholder }) => {
+const Input: React.FC<InputProps> = ({
+  id,
+  type,
+  placeholder,
+  errorMessage,
+  register,
+  name,
+  dirtyFields,
+}) => {
   return (
     <input
       type={type}
@@ -14,6 +26,7 @@ const Input: React.FC<InputProps> = ({ id, type, placeholder }) => {
       className='h-14  text-black bg-[#C3B6B2] text-sm  rounded-lg border-gray-400 focus:outline-none focus:border-gray-500  focus:ring-gray-500  block w-full p-4 border-2  placeholder:text-black '
       placeholder={placeholder}
       required
+      {...register(name)}
     />
   )
 }
