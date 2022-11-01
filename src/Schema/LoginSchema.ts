f550cbfffd4b370c8d806c83d4cbc58b1cb5e22f
Irakli <i.botchori@gmail.com>
2022-11-01 16:37:34 +0400

    fix: LoginSchema file

--- src/Schema/LoginSchema.ts
+++ src/Schema/LoginSchema.ts
@@ -5,15 +5,17 @@ export interface LoginInputTypes {
   password: string
 }
 
-export const LoginSchema = yup.object({
-  username: yup
-    .string()
-    .required('usernameRequired')
-    .min(3, 'usernameAtLeast')
-    .max(20, 'usernameMax'),
-  password: yup
-    .string()
-    .required('passwordRequired')
-    .min(3, 'passwordAtLeast')
-    .max(20, 'passwordMax'),
-})
+export const LoginSchema = yup
+  .object({
+    username: yup
+      .string()
+      .required('usernameRequired')
+      .min(3, 'usernameAtLeast')
+      .max(20, 'usernameMax'),
+    password: yup
+      .string()
+      .required('passwordRequired')
+      .min(3, 'passwordAtLeast')
+      .max(20, 'passwordMax'),
+  })
+  .required()
