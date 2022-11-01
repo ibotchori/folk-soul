2af16cc51187b55e8b89abaae7d497ba40b60970
Irakli <i.botchori@gmail.com>
2022-11-01 16:37:01 +0400

    feat: add LoginSchema file

---
+++ src/Schema/LoginSchema.ts
@@ -0,0 +1,19 @@
+import * as yup from 'yup'
+
+export interface LoginInputTypes {
+  username: string
+  password: string
+}
+
+export const LoginSchema = yup.object({
+  username: yup
+    .string()
+    .required('usernameRequired')
+    .min(3, 'usernameAtLeast')
+    .max(20, 'usernameMax'),
+  password: yup
+    .string()
+    .required('passwordRequired')
+    .min(3, 'passwordAtLeast')
+    .max(20, 'passwordMax'),
+})
