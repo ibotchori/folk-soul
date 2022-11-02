91768cf1da03ee8a0872fcb653fe11f2fd1f3e86
Irakli <i.botchori@gmail.com>
2022-11-02 19:08:34 +0400

    wip:  add on submit on Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -68,7 +68,10 @@ const Login = (props: Props) => {
     }
   }
   return (
-    <div className='  items-center justify-center flex w-full h-full'>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='  items-center justify-center flex w-full h-full'
+    >
       <div className='bg-gradient-to-t px-14 from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
         <div className='bg-[#EB5757] w-40 h-12 text-center skew-x-[-40deg]'>
           <p className='skew-x-[40deg] font-[mtavruli] font-bold pt-3 pr-2 text-md'>
@@ -92,14 +95,11 @@ const Login = (props: Props) => {
           errorMessage={errors.password?.message}
         />
 
-        <Link
-          to={'/dashboard'}
-          className='bg-[#10191d] border pt-4 rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli]  tracking-wider'
-        >
+        <button className='bg-[#10191d] border rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli]  tracking-wider'>
           შემობრძანდი
-        </Link>
+        </button>
       </div>
-    </div>
+    </form>
   )
 }
 
