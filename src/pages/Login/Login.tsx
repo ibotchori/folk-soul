a520095ec199121174f43ff057b72dcc9818b7e0
Irakli Botchorishvili <i.botchori@gmail.com>
2022-11-04 22:07:26 +0400

    chore: add temp username & password on login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -66,38 +66,45 @@ const Login = (props: Props) => {
     }
   }
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className='  items-center justify-center flex w-full h-full'
-    >
-      <div className='bg-gradient-to-t px-14 from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
-        <div className='bg-[#EB5757] w-40 h-12 text-center skew-x-[-40deg]'>
-          <p className='skew-x-[40deg] font-[mtavruli] font-bold pt-3 pr-2 text-md'>
-            კარიბჭე
-          </p>
-        </div>
-        <Input
-          name='username'
-          id='username'
-          placeholder='მეტსახელი'
-          type='text'
-          register={register}
-          errorMessage={errors.username?.message}
-        />
-        <Input
-          name='password'
-          id='password'
-          placeholder='პაროლი'
-          type='password'
-          register={register}
-          errorMessage={errors.password?.message}
-        />
-
-        <button className='bg-[#10191d] border rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli]  tracking-wider'>
-          შემობრძანდი
-        </button>
+    <>
+      <div className='absolute text-gray-300 m-auto   pt-5 pl-5  '>
+        <p>Username: daniel</p>
+        <p>Password: 123123</p>
       </div>
-    </form>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='  items-center justify-center flex w-full h-full'
+      >
+        <div className='bg-gradient-to-t px-14 from-[#7b5a5a] to-[#345161]   border rounded-sm  border-gray-200 w-[21rem] sm:w-[25rem] h-[27rem] items-center flex justify-evenly flex-col'>
+          <div className='bg-[#EB5757] w-40 h-12 text-center skew-x-[-40deg]'>
+            <p className='skew-x-[40deg] font-[mtavruli] font-bold pt-3 pr-2 text-md'>
+              კარიბჭე
+            </p>
+          </div>
+          <Input
+            name='username'
+            id='username'
+            placeholder='მეტსახელი'
+            type='text'
+            register={register}
+            errorMessage={errors.username?.message}
+          />
+          <Input
+            name='password'
+            id='password'
+            placeholder='პაროლი'
+            type='password'
+            register={register}
+            errorMessage={errors.password?.message}
+          />
+
+          <button className='bg-[#10191d] border rounded-md text-gray-300 border-gray-300  w-60 h-14 text-center font-[mtavruli]  tracking-wider'>
+            შემობრძანდი
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
 
